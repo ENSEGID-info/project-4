e6656c75f46de7fbe9be369d840a7e37ea8f1469
Havard832 <ehavard@gmx.fr>
2025-11-05 14:48:43 +0100

    Ajout des imports

--- etape1/etape1.py
+++ etape1/etape1.py
@@ -5,3 +5,18 @@ def etape1_main():
     print("Input data is " + str(data) + ".")
     return data
 
+
+# -----------------------------------------------------------
+# Analyse de glissements de terrain à partir de données sismiques
+# Inspiré de Piantini et al. (2021)
+
+# -----------------------------------------------------------
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr #en fait c'est l'histoire de ce qu'on a fait en stat, avec les corrélations donc si c'est >0 les variables varient dans le même sens et sinon en sens inverse
+
+# -----------------------------------------------------------
+# 1. Chargement des données
+# -----------------------------------------------------------
