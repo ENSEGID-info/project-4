cada1d9d076cfa4c7bd42c6aafc8c00c12ca080d
Honorine33 <honorine.heurtier@gmail.com>
2025-12-04 12:20:20 +0100

    Création fonction figure 7

--- etape2/etape2.py
+++ etape2/etape2.py
@@ -28,10 +28,12 @@ import os
 # ---------------------------------------------------------------
 
 # 📍 🔽 Modifie ici les noms de tes fichiers 🔽
-file_times_samples = "E:\ENSEGID\Projet de programmation\Figure 6\Fig6_time_samples.txt"
-file_Qs_samples = "E:\ENSEGID\Projet de programmation\Figure 6\Fig6_Qs_samples.txt"
-file_times_flow_stage = "E:\ENSEGID\Projet de programmation\Figure 6\Fig6_time_flow_stage.txt"
-file_h_flow_stage = "E:\ENSEGID\Projet de programmation\Figure 6\Fig6_h_flow_stage.txt"
+file_times_samples = "E:\ENSEGID\Projet de programmation\Data\Figure 6\Fig6_time_samples.txt"
+file_Qs_samples = "E:\ENSEGID\Projet de programmation\Data\Figure 6\Fig6_Qs_samples.txt"
+file_times_flow_stage = "E:\ENSEGID\Projet de programmation\Data\Figure 6\Fig6_time_flow_stage.txt"
+file_h_flow_stage = r"E:\ENSEGID\Projet de programmation\Data\Figure 6\Fig6_h_flow_stage.txt"
+
+
 
 # Vérification de l’existence des fichiers
 for f in [file_times_samples, file_Qs_samples, file_times_flow_stage, file_h_flow_stage]:
@@ -136,77 +138,96 @@ creation_du_graphique(times_flow_stage, h_flow_stage, no_measure_periods, Qs_sam
 
 # Programme figure 7
 
-# -------------------------------------------------------------------
-# 1) CHARGEMENT DES DONNEES
-# -------------------------------------------------------------------
+def figure_7 (file_gsd,file_times):
+    """
+    
 
+    Parameters
+    ----------
+    file_gsd : TYPE
+        DESCRIPTION.
+    file_times : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    # -------------------------------------------------------------------
+    # 1) CHARGEMENT DES DONNEES
+    # -------------------------------------------------------------------
+
+    GSD = np.loadtxt(file_gsd)
+    times = np.loadtxt(file_times)
+    
+    if GSD.shape[0] != len(times):
+        raise ValueError("Nombre de lignes GSD != nombre de temps.")
+    
+    # -------------------------------------------------------------------
+    # 2) DEFINITIONS DES CLASSES GRANULOMETRIQUES ET COULEURS
+    # -------------------------------------------------------------------
+    
+    grain_classes = [
+        "D > 12.5 mm",
+        "10 mm < D < 12.5 mm",
+        "8 mm < D < 10 mm",
+        "6.3 mm < D < 8 mm",
+        "4 mm < D < 6.3 mm",
+        "3.15 mm < D < 4 mm",
+        "2 mm < D < 3.15 mm",
+        "D < 2 mm"
+    ]
+    
+    colors = [
+        "#1a1a1a",
+        "#4c2b1f",
+        "#6b3c25",
+        "#8d522d",
+        "#b56a2d",
+        "#d68d3a",
+        "#f2b45b",
+        "#ffcc80"
+    ]
+    
+    # -------------------------------------------------------------------
+    # 3) FIGURE PRINCIPALE : GSD EMPILEE AVEC BARRES EPAISSES
+    # -------------------------------------------------------------------
+    
+    fig, ax = plt.subplots(figsize=(14,6))
+    
+    # Largeur des barres optimisée pour 66 points
+    bar_width = 5 # valeurs >0.8 rendent les barres plus épaisses
+    
+    # Empilement des barres avec contours noirs
+    ax.bar(times, GSD[:,0], color=colors[0], edgecolor='black', linewidth=0.8, width=bar_width, label=grain_classes[0])
+    bottom = GSD[:,0].copy()
+    for i in range(1, GSD.shape[1]):
+        ax.bar(times, GSD[:,i], bottom=bottom, color=colors[i],
+               edgecolor='black', linewidth=0.5, width=bar_width, label=grain_classes[i])
+        bottom += GSD[:,i]
+    
+    # Labels et titre
+    ax.set_xlabel("Time (s)", fontsize=12)
+    ax.set_ylabel("Q$_s$ (g s$^{-1}$)", fontsize=12)
+    ax.set_title("Solid Discharge and Grain Size Distribution ", fontsize=14)
+    ax.set_xlim(times.min(), times.max())
+    
+    # Limites de l'axe du temps étendues jusqu'à 1977
+    ax.set_xlim(400, 1450)
+    ax.set_ylim(0,400)
+    
+    
+    # Légende
+    ax.legend(fontsize=15, framealpha=1, loc='upper center', ncol=1)
+    
+    plt.tight_layout()
+    plt.show()
+
+
+    
 # Fichiers principaux
 file_gsd = r"E:\ENSEGID\Projet de programmation\Figure 7\Fig7_Qs_GSD.txt"              # GSD (66 x 8)
 file_times = r"E:\ENSEGID\Projet de programmation\Figure 7\Fig7_Time_samples.txt"              # Temps (66 lignes)
-
-
-GSD = np.loadtxt(file_gsd)
-times = np.loadtxt(file_times)
-
-if GSD.shape[0] != len(times):
-    raise ValueError("Nombre de lignes GSD != nombre de temps.")
-
-# -------------------------------------------------------------------
-# 2) DEFINITIONS DES CLASSES GRANULOMETRIQUES ET COULEURS
-# -------------------------------------------------------------------
-
-grain_classes = [
-    "D > 12.5 mm",
-    "10 mm < D < 12.5 mm",
-    "8 mm < D < 10 mm",
-    "6.3 mm < D < 8 mm",
-    "4 mm < D < 6.3 mm",
-    "3.15 mm < D < 4 mm",
-    "2 mm < D < 3.15 mm",
-    "D < 2 mm"
-]
-
-colors = [
-    "#1a1a1a",
-    "#4c2b1f",
-    "#6b3c25",
-    "#8d522d",
-    "#b56a2d",
-    "#d68d3a",
-    "#f2b45b",
-    "#ffcc80"
-]
-
-# -------------------------------------------------------------------
-# 3) FIGURE PRINCIPALE : GSD EMPILEE AVEC BARRES EPAISSES
-# -------------------------------------------------------------------
-
-fig, ax = plt.subplots(figsize=(14,6))
-
-# Largeur des barres optimisée pour 66 points
-bar_width = 5 # valeurs >0.8 rendent les barres plus épaisses
-
-# Empilement des barres avec contours noirs
-ax.bar(times, GSD[:,0], color=colors[0], edgecolor='black', linewidth=0.8, width=bar_width, label=grain_classes[0])
-bottom = GSD[:,0].copy()
-for i in range(1, GSD.shape[1]):
-    ax.bar(times, GSD[:,i], bottom=bottom, color=colors[i],
-           edgecolor='black', linewidth=0.5, width=bar_width, label=grain_classes[i])
-    bottom += GSD[:,i]
-
-# Labels et titre
-ax.set_xlabel("Time (s)", fontsize=12)
-ax.set_ylabel("Q$_s$ (g s$^{-1}$)", fontsize=12)
-ax.set_title("Solid Discharge and Grain Size Distribution ", fontsize=14)
-ax.set_xlim(times.min(), times.max())
-
-# Limites de l'axe du temps étendues jusqu'à 1977
-ax.set_xlim(400, 1450)
-ax.set_ylim(0,400)
-
-
-# Légende
-ax.legend(fontsize=15, framealpha=1, loc='upper center', ncol=1)
-
-plt.tight_layout()
-plt.show()
+figure_7 (file_gsd,file_times) 
+    
